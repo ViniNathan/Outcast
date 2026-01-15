678bf838523bb47e472d33f75f197e3a20150235
ViniNathan <viniciosnathan@gmail.com>
2026-01-15 16:14:31 -0300

    refactor: simplify NextAuth handler export in TypeScript

--- frontend/src/pages/api/auth/[...nextauth].ts
+++ frontend/src/pages/api/auth/[...nextauth].ts
@@ -17,6 +17,4 @@ export const authOptions: AuthOptions = {
   // callbacks: { ... }
 };
 
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
+export default NextAuth(authOptions)
